86c1ad3d2483e8c4d433c4a837b3dddfbacce14a
Ronal <ronalps075@gmail.com>
2024-06-13 22:08:28 +0700

    Refactor

--- utils/tokenUtils.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../config/jwt');
-
-function generateToken(user) {
-  return jwt.sign({ uid: user.uid, email: user.email }, JWT_SECRET, { expiresIn: '60d' });
-}
-
-module.exports = { generateToken };
